ff1a906d0f143d6c9838ff424ee142fef2228f6b
HJ <117727017+HJ-dp@users.noreply.github.com>
2024-10-18 03:53:56 +0900

    Title: 개발팀의 능력, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241018/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Main {
+    public static int[] arr = new int[5];
+    public static int max;
+    public static int getDiff(int x, int y, int z, int k){
+        int sum1 = arr[x] + arr[y];
+        int sum2 = arr[z] + arr[k];
+        int sum3 = max - arr[z] - arr[k] - arr[x] - arr[y];
+        if(sum1 != sum2 && sum2 != sum3 && sum1 != sum3){
+            return Math.abs(Math.max(sum1, Math.max(sum2, sum3)) - Math.min(sum1, Math.min(sum2, sum3)));
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            arr[i] = sc.nextInt();
+            max += arr[i];
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for(int i=0;i<5;i++){
+            for(int j=i+1;j<5;j++){
+                for(int k=j+1;k<5;k++){
+                    for(int z=k+1;z<5;z++){
+                        answer = Math.min(answer,getDiff(i,j,k,z));
+                    }
+                }
+            }
+        }
+        if(answer ==Integer.MAX_VALUE) System.out.println(-1);
+        else {
+            System.out.println(answer);
+        }
+    }
+}
