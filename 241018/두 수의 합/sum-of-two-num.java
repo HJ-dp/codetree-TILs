d0ebbeb001c010aead19cd8bd2e500c1a72e59cd
HJ <117727017+HJ-dp@users.noreply.github.com>
2024-10-18 22:24:35 +0900

    Title: 두 수의 합, Time: 3016ms, Memory: 152MB, Status: Time Limit Exceed - CodeTree

--- 241018/두 수의 합/sum-of-two-num.java
+++ 241018/두 수의 합/sum-of-two-num.java
@@ -1,7 +1,15 @@
 import java.util.*;
+
+class Pair{
+    int x,y;
+    public Pair(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+}
 public class Main {
     static int N, K, answer;
-    static HashMap<Integer,Integer> hs = new HashMap<>();
+    static HashMap<Pair,Integer> hs = new HashMap<>();
     static HashMap<Integer,Integer> hss = new HashMap<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,8 +23,10 @@ public class Main {
         for(int i=0;i<N;i++){
             int temp = hss.get(i);
             for(int j=i+1;j<N;j++){
-                if(hss.get(i) + hss.get(j) == K){
+                if(hss.get(i) + hss.get(j) == K && hs.get(new Pair(i,j)) == null){
                     answer++;
+                    hs.put(new Pair(i,j),1);
+                    hs.put(new Pair(j,i),1);
                 }
             }
         }
