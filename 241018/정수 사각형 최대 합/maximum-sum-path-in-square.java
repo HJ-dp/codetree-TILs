4751a9c47e9e83228a5d84e578db0377f10abe72
HJ <117727017+HJ-dp@users.noreply.github.com>
2024-10-18 17:04:28 +0900

    Title: 정수 사각형 최대 합, Time: 3056ms, Memory: 22MB, Status: Time Limit Exceed - CodeTree

--- 241018/정수 사각형 최대 합/maximum-sum-path-in-square.java
+++ 241018/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -25,7 +25,7 @@ public class Main {
     }
 
     public static void casa(int x, int y, int cnt, int c){
-        if(c > N*2){continue;}
+        if(c > N*2){return;}
         if(x == N-1 && y == N-1){
             max = Math.max(max, cnt + map[N-1][N-1]);
             return;
