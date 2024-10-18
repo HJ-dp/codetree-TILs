ce00432e1bfe2128479812c090a9ab76ec8852f0
HJ <117727017+HJ-dp@users.noreply.github.com>
2024-10-18 17:02:01 +0900

    Title: 정수 사각형 최대 합, Time: 3076ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

---
+++ 241018/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Main {
+    static int N, max = 0;
+    static int[][] map;
+    static int[] dx = new int[]{0,1};
+    static int[] dy = new int[]{1,0};
+    static boolean[][] visited;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        map = new int[N][N];
+        visited = new boolean[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                map[i][j] = sc.nextInt();
+            }
+        }
+        casa(0,0,0);
+        
+        System.out.println(max);
+
+    }
+    public static boolean inRange(int x, int y){
+        return 0 <= x && x <N && 0 <= y && y < N;
+    }
+
+    public static void casa(int x, int y, int cnt){
+        // visited[x][y] = true;
+        if(x == N-1 && y == N-1){
+            max = Math.max(max, cnt + map[N-1][N-1]);
+            return;
+        }
+        cnt += map[x][y];
+        for(int i=0;i<2;i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(inRange(nx,ny)){
+                casa(nx,ny,cnt);
+            } else {
+                return;
+            }
+        
+        }
+    }
+}
