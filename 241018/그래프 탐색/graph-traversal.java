4fdb6ed071f934629fab469006332211814245c5
HJ <117727017+HJ-dp@users.noreply.github.com>
2024-10-18 13:51:22 +0900

    Title: 그래프 탐색, Time: 489ms, Memory: 24MB, Status: Passed - CodeTree

--- 241018/그래프 탐색/graph-traversal.java
+++ 241018/그래프 탐색/graph-traversal.java
@@ -1,35 +1,40 @@
 import java.util.*;
 
 public class Main {
+    static final int MAX_NUM = 1000;
     static int N, M, answer = 0;
-    static int[][] map;
-    static boolean[] visited;
+    
+    static boolean[] visited = new boolean[MAX_NUM + 1];
+    static ArrayList<Integer>[] map = new ArrayList[MAX_NUM+1];
+
+    public static void DFS(int v){
+        for(int i=0; i < map[v].size(); i++){
+            int cur = map[v].get(i);
+            if(!visited[cur]){
+                visited[cur] = true;
+                answer++;
+                DFS(cur);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-        map = new int[N+1][N+1];
-        visited = new boolean[N+1];
+        for(int i=1;i<=N;i++){
+            map[i] = new ArrayList<>();
+        }
 
         for(int i=0;i<M;i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
-            map[a][b] = 1;
-            map[b][a] = 1;
+            map[a].add(b);
+            map[b].add(a);
         }
         visited[1] = true;
         DFS(1);
 
         System.out.println(answer);
-    }
-
-    public static void DFS(int v){
-        for(int i=1;i<=N;i++){
-            if(map[v][i]==1 && !visited[i]){
-                visited[i] = true;
-                answer++;
-                DFS(i);
-            }
-        }
     }
 }
